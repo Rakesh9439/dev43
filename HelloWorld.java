a8677b90b37d48432d51f0e7b35f83786a5314e3
Rakesh Kumar <webmkumar@gmail.com>
2026-04-25 10:04:26 +0530

    Resolved merge conflict in HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,9 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("Hello, World! this is my first java program");
-=======
         System.out.println("Hello, Rakesh is my first job example test code");
->>>>>>> e0f0a6b32e84dd2404cfeddb24931e49ac246ee1
+
     }
 }
