748391aa7cd70e2d1b1b24b7570e8faba88d0ece
Rakesh Kumar <webmkumar@gmail.com>
2026-04-29 22:32:55 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello, World! this is my first java program  first program add success hii this do it");
-        System.out.println("Hello, Rakesh is my first job example test code pipeline running due to market so due to condition");
+        System.out.println("Hello, Rakesh is my first job example test code pipeline running due to market so due to condition test");
 
     }
 }
